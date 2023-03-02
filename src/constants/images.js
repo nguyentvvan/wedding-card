e69971521c10e76115720b16352061231c76e884
Van Nguyen <nguyentvvan@gmail.com>
2023-03-02 21:18:37 +0700

    adding banner

---
+++ src/constants/images.js
@@ -0,0 +1,9 @@
+import bannerBackground from 'assets/images/orange-bg.jpg';
+import ring from 'assets/images/ring.png';
+
+const Images = {
+  BANNER_BG: bannerBackground,
+	RING: ring,
+}
+
+export default Images;
